790475b7ad1d5736b4ca7db5d34fc01f6156f64a
Turner Monroe <me@turnercore.dev>
2024-10-05 13:51:45 +0200

    Refactor EventBus

--- Assets/Menus/WackableButton.cs
+++ Assets/Menus/WackableButton.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class WackableButton : MonoBehaviour
 {
+    private bool isWackable = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Subscribe to the EventBus, OnEnableWackableButtons event
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+}
+
+public enum ButtonTypes
+{
+    Play,
+    Quit,
+    Resume,
+    Restart,
+    MainMenu,
+    Options,
+    Credits
 }
--- Assets/Singletons/EventBus.cs
+++ Assets/Singletons/EventBus.cs
@@ -1,153 +1,78 @@
 using UnityEngine;
-using System; // For Action and EventHandler
+using System;
 
 public class EventBus : MonoBehaviour
 {
-    // The static instance of EventBus
     public static EventBus Instance { get; private set; }
 
-    // Called when the script instance is being loaded
     private void Awake()
     {
-        // Detach from any parent object
         transform.SetParent(null);
-        // If no other instance exists, make this the singleton instance
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Make persistent across scenes
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
-            Destroy(gameObject); // Destroy duplicate EventBus objects
+            Destroy(gameObject);
         }
     }
 
-    // Example of event declaration using Action
-
-    /*
-    Global events can be declared here, and other scripts can subscribe to them.
-    When the event is triggered, all subscribed methods will be called.
-
-    Events:
-    - OnPlayerDied: Triggered when the player dies
-    - OnPlayerHit: Triggered when the player is hit
-    - OnEnemyDied: Triggered when an enemy dies
-    - OnGameWon: Triggered when the game is won
-    - OnGameOver: Triggered when the game is over
-    - OnGamePaused: Triggered when the game is paused
-    - OnGameResumed: Triggered when the game is resumed
-    - OnXPChanged: Triggered when the player's XP changes
-    - OnLevelUp: Triggered when the player levels up
-    - OnCoinCollected: Triggered when a coin is collected
-
-    */
-
-    // OnPlayerDied event trigger
+    // Player events
     public event Action OnPlayerDied;
-    public void PlayerDied()
-    {
-        OnPlayerDied?.Invoke(); // Safely invoke event
-    }
-    public void SubscribeToPlayerDeath(Action listener)
-    {
-        OnPlayerDied += listener;
-    }
-    public void UnsubscribeFromPlayerDeath(Action listener)
-    {
-        OnPlayerDied -= listener;
-    }
+    public void TriggerPlayerDied() => OnPlayerDied?.Invoke();
 
-    // OnPlayerHit event trigger
     public event Action<float, Vector2> OnPlayerHit;
-    public void PlayerHit(float damage, Vector2 hitDirection)
-    {
-        OnPlayerHit?.Invoke(damage, hitDirection); // Safely invoke event
-    }
-    public void SubscribeToPlayerHit(Action<float, Vector2> listener)
-    {
-        OnPlayerHit += listener;
-    }
-    public void UnsubscribeFromPlayerHit(Action<float, Vector2> listener)
-    {
-        OnPlayerHit -= listener;
-    }
+    public void TriggerPlayerHit(float damage, Vector2 hitDirection) => OnPlayerHit?.Invoke(damage, hitDirection);
 
-    // OnEnemyDied event trigger
+    // Enemy events
     public event Action<Enemy> OnEnemyDied;
-    public void EnemyDied(Enemy enemy)
+    public void TriggerEnemyDied(Enemy enemy)
     {
         OnEnemyDied?.Invoke(enemy);
-        KillsTextUpdate(); // Trigger KillsTextUpdate event
-    }
-    public void SubscribeToEnemyDeath(Action<Enemy> listener)
-    {
-        OnEnemyDied += listener;
+        TriggerKillsTextUpdate();
     }
 
-    public void UnsubscribeFromEnemyDeath(Action<Enemy> listener)
-    {
-        OnEnemyDied -= listener;
-    }
+    // Game state events
+    public event Action OnGameWon;
+    public void TriggerGameWon() => OnGameWon?.Invoke();
 
-    // OnXPChanged event trigger
+    public event Action OnGameOver;
+    public void TriggerGameOver() => OnGameOver?.Invoke();
+
+    public event Action OnGamePaused;
+    public void TriggerGamePaused() => OnGamePaused?.Invoke();
+
+    public event Action OnGameResumed;
+    public void TriggerGameResumed() => OnGameResumed?.Invoke();
+
+    // XP and level events
     public event Action OnXPChanged;
-    public void XPChanged()
-    {
-        OnXPChanged?.Invoke(); // Safely invoke event
-    }
-    public void SubscribeToXPChanged(Action listener)
-    {
-        OnXPChanged += listener;
-    }
-    public void UnsubscribeFromXPChanged(Action listener)
-    {
-        OnXPChanged -= listener;
-    }
+    public void TriggerXPChanged() => OnXPChanged?.Invoke();
 
-    // OnLevelUp event trigger
     public event Action OnLevelUp;
-    public void LevelUp()
+    public void TriggerLevelUp()
     {
-        OnLevelUp?.Invoke(); // Safely invoke event
-        OnXPChanged?.Invoke(); // Trigger XPChanged event
-    }
-    public void SubscribeToLevelUp(Action listener)
-    {
-        OnLevelUp += listener;
-    }
-    public void UnsubscribeFromLevelUp(Action listener)
-    {
-        OnLevelUp -= listener;
+        OnLevelUp?.Invoke();
+        TriggerXPChanged();
     }
 
-    // OnCoinCollected event trigger
+    // Coin events
     public event Action<int> OnCoinCollected;
-    public void CoinCollected(int value)
-    {
-        OnCoinCollected?.Invoke(value); // Safely invoke event
-    }
-    public void SubscribeToCoinCollected(Action<int> listener)
-    {
-        OnCoinCollected += listener;
-    }
-    public void UnsubscribeFromCoinCollected(Action<int> listener)
-    {
-        OnCoinCollected -= listener;
-    }
+    public void TriggerCoinCollected(int value) => OnCoinCollected?.Invoke(value);
 
-    // OnKillsTextUpdate event trigger
+    // UI events
     public event Action OnKillsTextUpdate;
-    public void KillsTextUpdate()
-    {
-        OnKillsTextUpdate?.Invoke(); // Safely invoke event
-    }
-    public void SubscribeToKillsTextUpdate(Action listener)
-    {
-        OnKillsTextUpdate += listener;
-    }
-    public void UnsubscribeFromKillsTextUpdate(Action listener)
-    {
-        OnKillsTextUpdate -= listener;
-    }
+    public void TriggerKillsTextUpdate() => OnKillsTextUpdate?.Invoke();
+
+    // Wackable button events
+    public event Action OnWackableButtonsEnabled;
+    public void TriggerWackableButtonsEnabled() => OnWackableButtonsEnabled?.Invoke();
+
+    public event Action OnWackableButtonsDisabled;
+    public void TriggerWackableButtonsDisabled() => OnWackableButtonsDisabled?.Invoke();
+
+    public event Action<ButtonTypes> OnButtonWacked;
+    public void TriggerButtonWacked(ButtonTypes buttonType) => OnButtonWacked?.Invoke(buttonType);
 }
