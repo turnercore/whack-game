8c6369b52613acf52ecb926e60af8a3d16b6e440
Turner Monroe <me@turnercore.dev>
2024-10-07 11:19:17 +0200

    Removed unneeded TMP files

---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    // List of all the screens
+    public List<Screen> screens = new List<Screen>();
+}
